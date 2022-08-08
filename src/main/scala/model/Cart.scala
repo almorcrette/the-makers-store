9e4da194ba453eb848f22d641245b3979c895e44
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-08-08 16:06:47 +0100

    test&implement: cart.addItem with single item available in inventory in location

--- src/main/scala/model/Cart.scala
+++ src/main/scala/model/Cart.scala
@@ -1,10 +1,30 @@
 //import java.util.UUID
 import io.jvm.uuid._
 
-class Cart(val uuidFactory: FactoryBase[UUID] = UUIDFactory) {
-  val uuid = uuidFactory.create
+import scala.::
+import scala.collection.immutable.Map
 
-  def getUUID() = {
+class Cart(
+            val location: String,
+            val uuidFactory: FactoryBase[UUID] = UUIDFactory,
+            val itemsController: ItemsController = new ItemsController
+          ) {
+  val uuid = uuidFactory.create
+  private var items: Map[String, Int] = Map()
+
+  def getUUID(): UUID = {
     uuid
   }
+
+  def addItem(itemName: String): Unit = {
+    val availableItems = itemsController.retrieveByLocation(location)
+    if (availableItems.map(item => item.name).contains(itemName) && availableItems.filter(item => item.name == itemName).last.quantity > 0 ) {
+      items += (itemName -> 1)
+    }
+  }
+
+  def viewItems(): Map[String, Int] = {
+    items
+  }
+
 }
--- src/test/scala/model/CartSpec.scala
+++ src/test/scala/model/CartSpec.scala
@@ -1,7 +1,10 @@
 import io.jvm.uuid.{StaticUUID, UUID}
+import main.model.Item
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+
+import scala.collection.mutable.ArrayBuffer
 
 class CartSpec extends AnyWordSpec with Matchers with MockFactory {
   "Cart.getUUID" should {
@@ -11,8 +14,35 @@ class CartSpec extends AnyWordSpec with Matchers with MockFactory {
 
       (mockUuidFactory.create _).expects().anyNumberOfTimes.returning(mockUuid)
 
-      val cart = new Cart(mockUuidFactory)
+      val cart = new Cart("London", mockUuidFactory)
       cart.getUUID() should equal (mockUuid)
+    }
+  }
+
+  "Cart.addItem" should {
+
+    "add an item to the cart if it is available in the cart location and the item is in stock" in {
+      val mockUuidFactory = mock[FactoryBase[UUID]]
+      val mockUuid = UUIDFactory.create
+
+      (mockUuidFactory.create _).expects().anyNumberOfTimes.returning(mockUuid)
+
+      val mockItemsController = mock[ItemsController]
+
+      val scoop = new Item(2, "Icecream scoop", 4.95, 1000, List("Europe"))
+      val blender = new Item(3, "Blender", 44.50, 200, List("Europe", "NA"))
+      val breadMaker = new Item(4, "Bread maker", 99.99, 50, List("NA"))
+      val mockItemsInventory = ArrayBuffer(scoop, blender, breadMaker)
+
+      val cart = new Cart("London", mockUuidFactory, mockItemsController)
+      (mockItemsController.retrieveByLocation _).expects("London").returns(mockItemsInventory)
+
+      cart.addItem("Icecream scoop")
+
+      println(cart.viewItems())
+      println(cart.viewItems()("Icecream scoop"))
+
+      cart.viewItems()("Icecream scoop") should equal(1)
     }
   }
 }
