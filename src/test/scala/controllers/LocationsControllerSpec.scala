3690f5724dbfb443ac0c5257b07464d57df68118
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-08-09 11:21:06 +0100

    refactor: make LocationsControllerSpec more DRY

--- src/test/scala/controllers/LocationsControllerSpec.scala
+++ src/test/scala/controllers/LocationsControllerSpec.scala
@@ -8,65 +8,39 @@ import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 class LocationsControllerSpec extends AnyWordSpec with Matchers with MockFactory {
-  val aLocation = new Location(
-    0,
-    "Woodbridge"
+  val aLocation = new Location(0, "Woodbridge")
+  val anotherLocation = new Location(2, "Reading")
+  val aFrenchLocation = new Location(3, "Le Vigan")
+
+  val mockDbLocationsLinkedHashMap = mutable.LinkedHashMap(
+    "Europe" -> mutable.LinkedHashMap(
+      "UK" -> Seq(aLocation, anotherLocation),
+      "France" -> Seq(aFrenchLocation)
+    )
   )
-  val anotherLocation = new Location(
-    2,
-    "Reading"
-  )
-  val aFrenchLocation = new Location(
-    3,
-    "Le Vigan"
-  )
+
+  val mockDbAdapter = mock[DbAdapterBase]
+  val locationsController = new LocationsController(mockDbAdapter)
+
   "LocationsController.retrieveAll" should {
     "fetch all locations" in {
-      val mockDbAdapter = mock[DbAdapterBase]
-      val mockDbLocationsLinkedHashMap = mutable.LinkedHashMap(
-        "Europe" -> mutable.LinkedHashMap(
-          "UK" -> Seq(aLocation, anotherLocation),
-          "France" -> Seq(aFrenchLocation)
-        )
-      )
-      val locationsController = new LocationsController(mockDbAdapter)
-
       (mockDbAdapter.getLocations _).expects().returns(mockDbLocationsLinkedHashMap)
       locationsController.retrieveAll() should equal(mockDbLocationsLinkedHashMap)
     }
   }
+
   "LocationsController.retrieveByContinent" should {
     "fetch all locations given a continent" in {
-      val mockDbAdapter = mock[DbAdapterBase]
-      val mockDbLocationsLinkedHashMap = mutable.LinkedHashMap(
-        "Europe" -> mutable.LinkedHashMap(
-          "UK" -> Seq(aLocation, anotherLocation),
-          "France" -> Seq(aFrenchLocation)
-        )
-      )
-      val locationsController = new LocationsController(mockDbAdapter)
-
       (mockDbAdapter.getLocations _).expects().returns(mockDbLocationsLinkedHashMap)
       locationsController.retrieveByContinent("Europe") should equal(ArrayBuffer("Woodbridge", "Reading", "Le Vigan"))
     }
     "raises an error if continent given not found" in {
-      val mockDbAdapter = mock[DbAdapterBase]
-      val mockDbLocationsLinkedHashMap = mutable.LinkedHashMap(
-        "Europe" -> mutable.LinkedHashMap(
-          "UK" -> Seq(aLocation, anotherLocation),
-          "France" -> Seq(aFrenchLocation)
-        )
-      )
-      val locationsController = new LocationsController(mockDbAdapter)
-
       (mockDbAdapter.getLocations _).expects().returns(mockDbLocationsLinkedHashMap)
       val thrown = the [Exception] thrownBy {
         locationsController.retrieveByContinent("Antarctica")
       }
       thrown.getMessage should equal ("Continent not found")
-
-
     }
-
   }
+
 }
