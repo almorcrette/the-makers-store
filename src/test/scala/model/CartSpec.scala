f58b6a41611747c2b36d8f347ff4142d6fe00ab2
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-08-09 13:50:00 +0100

    test: add test to cover cart.changeAmount by more than 1

--- src/test/scala/model/CartSpec.scala
+++ src/test/scala/model/CartSpec.scala
@@ -107,7 +107,18 @@ class CartSpec extends AnyWordSpec with Matchers with MockFactory with BeforeAnd
       cart.addItem("icecream scoop")
       cart.changeAmount("iceCreaM ScOOP", 1, "-")
       cart.viewItems()("icecream scoop") should equal(0)
-
+    }
+    "increase the amount of an item in the cart by more than 1 (regardless of how the item is typed)" in {
+      (mockItemsController.retrieveByLocation _).when("London").returns(londonInventory)
+      cart.addItem("icecream scoop")
+      cart.changeAmount("iceCreaM ScOOP", 3)
+      cart.viewItems()("icecream scoop") should equal(4)
+    }
+    "decrease the amount of an item in the cart by more than 1 (regardless of how the item is typed)" in {
+      (mockItemsController.retrieveByLocation _).when("London").returns(londonInventory)
+      cart.addItem("icecream scoop", 3)
+      cart.changeAmount("iceCreaM ScOOP", 3, "-")
+      cart.viewItems()("icecream scoop") should equal(0)
     }
     "raise an error if the item is not found in the cart" in {
 
